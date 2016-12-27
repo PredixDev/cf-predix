9bcd01fdc8d7ac586650658275604a89f8f0cec5
Sabith K Soopy <sabith.karippullilsoopy@ge.com>
2016-12-26 19:15:51 -0800

    Adding travis configuration

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/bin/bash -exu
 
 rm -rf bin
-mkdir bin
-GOOS=darwin GOARCH=amd64 go build -o "bin/osx/predix"
-GOOS=linux GOARCH=amd64 go build -o "bin/linux64/predix"
-GOOS=windows GOARCH=amd64 go build -o "bin/win64/predix.exe"
-GOOS=windows GOARCH=386 go build -o "bin/win32/predix.exe"
-
+mkdir -p bin
+GOOS=darwin GOARCH=amd64 go build -o "bin/predix_darwin_64"
+GOOS=linux GOARCH=amd64 go build -o "bin/predix_linux_64"
+GOOS=windows GOARCH=amd64 go build -o "bin/predix_win_64.exe"
+GOOS=windows GOARCH=386 go build -o "bin/predix_win_32.exe"
